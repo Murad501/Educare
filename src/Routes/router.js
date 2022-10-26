53ba8143cbd2222b1610e62529b2533a26bdf02d
Murad501 <mm845635@gmail.com>
2022-10-27 00:50:04 +0600

    404 page added

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -6,58 +6,79 @@ import CourseDetailsFull from "../components/CourseDetailsFull/CourseDetailsFull
 import Courses from "../components/Courses/Courses";
 import FAQ from "../components/FAQ/FAQ";
 import LogIn from "../components/LogIn/LogIn";
+import Page404 from "../components/Page404/Page404";
 import Register from "../components/Register/Register";
 import Main from "../Layout/Main";
 import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                loader: async()=> fetch('https://assignment-server-site-murad501.vercel.app/allCourses'),
-                element: <Courses></Courses>
-            },
-            {
-                path: 'courses',
-                loader: async()=> fetch('https://assignment-server-site-murad501.vercel.app/allCourses'),    
-                element: <Courses></Courses>
-            },
-            {
-                path: 'category/:id',
-                loader: async({params})=> fetch(`https://assignment-server-site-murad501.vercel.app/category/${params.id}`),    
-                element: <CategoryPage></CategoryPage>
-            },
-            {
-                path: 'course/:id',
-                loader: async({params})=> fetch(`https://assignment-server-site-murad501.vercel.app/course/${params.id}`),    
-                element: <CourseDetailsFull></CourseDetailsFull>
-            },
-            {
-                path: 'course/checkout/:id',
-                loader: async({params})=> fetch(`https://assignment-server-site-murad501.vercel.app/course/${params.id}`),    
-                element: <PrivateRoute><CheckOut></CheckOut></PrivateRoute>
-            },
-            {
-                path: 'faq',
-                element: <FAQ></FAQ>
-            },
-            {
-                path: 'blog',
-                element: <Blog></Blog>
-            },
-            {
-                path: 'login',
-                element: <LogIn></LogIn>
-            },
-            {
-                path: 'register',
-                element: <Register></Register>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Page404></Page404>,
+    children: [
+      {
+        path: "/",
+        loader: async () =>
+          fetch(
+            "https://assignment-server-site-murad501.vercel.app/allCourses"
+          ),
+        element: <Courses></Courses>,
+      },
+      {
+        path: "courses",
+        loader: async () =>
+          fetch(
+            "https://assignment-server-site-murad501.vercel.app/allCourses"
+          ),
+        element: <Courses></Courses>,
+      },
+      {
+        path: "category/:id",
+        loader: async ({ params }) =>
+          fetch(
+            `https://assignment-server-site-murad501.vercel.app/category/${params.id}`
+          ),
+        element: <CategoryPage></CategoryPage>,
+      },
+      {
+        path: "course/:id",
+        loader: async ({ params }) =>
+          fetch(
+            `https://assignment-server-site-murad501.vercel.app/course/${params.id}`
+          ),
+        element: <CourseDetailsFull></CourseDetailsFull>
+      },
+      {
+        path: "checkout/:id",
+        loader: async ({ params }) =>
+          fetch(
+            `https://assignment-server-site-murad501.vercel.app/course/${params.id}`
+          ),
+        element: (
+          <PrivateRoute>
+            <CheckOut></CheckOut>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "faq",
+        element: <FAQ></FAQ>,
+      },
+      {
+        path: "blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "login",
+        element: <LogIn></LogIn>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
 
-export default router
+export default router;
